aedc184625a960c038b9156772af6d78b2017367
AnatoliySt20 <strikun.a.p@gmail.com>
2016-12-25 17:04:21 +0200

    issue 25

--- main.py
+++ main.py
@@ -125,11 +125,23 @@ def moving_bone(bones_on_table, bones):
         if left in points or right in points:
             return index, points
 
-def add_player_to_dict_of_players(players_bones_list):
+
+def add_player_to_dict_of_players():
     counter=0
-    for bones in player_bones_list:
-        players_dict[counter]['bones']=bones
+    for player in range(players_now_num):
+        players_dict[counter]['bones']=[]
+        players_dict[counter][ 'priority_move']=None
         counter +=1
+
+
+def add_bones_to_dict_of_players():
+    if len(players_bones)!=players_now_num:
+        raise ValueError('not enough players')
+        for player in range(players_now_num):
+            players_dict[player]['bones']=players_bones[player]
+
+
+def add_priority_to_dict_of_players()
     first_player_index=get_first_move_player(players_bones_list)
     for player in players_dict:
         players_dict[player][ 'priority_move']=(player+first_player_index)%len(players_dict)
